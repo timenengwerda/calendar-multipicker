84f016a8e9bf6fd55928a6e8ec9a25a53ee9f437
Timen Engwerda <timenengwerda@MBP-van-Timen.fritz.box>
2017-01-26 14:22:40 +0100

    Add previous month days to fill the front of current month Add next monty days to fill the back of the current month

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,41 +1,50 @@
 import moment from 'Moment'
 
 
-
-const year = 2017
+const now = moment()
+console.log(now.format())
+const year = now.year()
 const calendarEl = $('.calendar');
 let firstIteration = true;
-let activeMonth = 0; // The month you are looking at at this point. Is defaulted to january for now
-let activeYear = 2017;
+let activeMonth = now.month(); // The month you are looking at at this point. Is defaulted to january for now
+let activeYear = year;
 
-for (let currentMonth = activeMonth; currentMonth <= 1; currentMonth++) {
+for (let currentMonth = activeMonth; currentMonth < 1; currentMonth++) {
     const month = moment([year, currentMonth]);
 
     let monthEl = $('<div />').addClass('month');
     monthEl.data('month', currentMonth);
     monthEl.data('year', year);
 
-    // add blank days until the first day of the month is reached
-    // i.e if february 1 starts on a wednesday the monday and tuesday before that should be greyed out
     let firstDayOfMonthNumber = ((month.date(1).weekday() - 1) + 8);
     firstDayOfMonthNumber = (firstDayOfMonthNumber > 7) ? firstDayOfMonthNumber - 7 : firstDayOfMonthNumber;
-    for (var i = 1; i < firstDayOfMonthNumber; i++) {
-        let dayEl = $('<div/>').addClass('day').addClass('inactive');
+
+    // get all the days before the first of this month to pre-fill the month (If january 1 is on a wednesday, fill monday and tuesday with 30 and 31 dec)
+    const lastMonth = getLastMonth(year, currentMonth)
+    const lastDayOfLastMonth = lastMonth.endOf('month').format('D');
+    const daysInThisWeekFromLastMonth = (lastDayOfLastMonth - firstDayOfMonthNumber) + 2; // compensate 1 because sunday is day 0
+    for (var i = daysInThisWeekFromLastMonth; i <= lastDayOfLastMonth; i++) {
+        const thisDate = lastMonth.date(i);
+
+        let dayEl = createDayElement(thisDate);
+        dayEl.addClass('prev-month');
         monthEl.append(dayEl);
     }
 
-    /*
-        Use a day iterator to see where the month ends(31 january is on wednesday for example,
-        we want to know this so we can add blank days up until the following sunday)
-    */
-
     let dayIterator = firstDayOfMonthNumber;
 
+    /*
+        Use this to define which day in this month is last.
+        We use this later on to decide how many days of the next month should be appended to this month
+    */
+    let lastDay = false;
     for (let i = 1; i <= month.daysInMonth(); i++) {
         const thisDate = moment([year, currentMonth]).date(i);
+        if (thisDate >= now) {
+            console.log(thisDate.format());
+        }
 
-        let dayEl = createDayElement(thisDate)p
-
+        let dayEl = createDayElement(thisDate);
         monthEl.append(dayEl);
         monthEl.hide();
 
@@ -44,7 +53,8 @@ for (let currentMonth = activeMonth; currentMonth <= 1; currentMonth++) {
         } else {
             ++dayIterator;
         }
-        // console.log(dayIterator);
+
+        lastDay = thisDate;
     }
 
     if (firstIteration) {
@@ -55,18 +65,40 @@ for (let currentMonth = activeMonth; currentMonth <= 1; currentMonth++) {
         monthEl.show();
     }
 
-    // See how many days it iterated this week. Subtract this by 7(The number of days in a week)
-    for (var nextMonthDay = 1; nextMonthDay <= (7 - dayIterator); nextMonthDay++) {
-        console.log(moment([year, currentMonth + 1, nextMonthDay]).format() +'<--');
+    /*
+        we have saved the lastDay of the month iteration. So we can just count back from 7 to the last day in the month
+        to figure out how many days from the next month should be added to this month's calendar
+    */
+    for (var nextMonthDay = 1; nextMonthDay <= (7 - lastDay.weekday()); nextMonthDay++) {
+        const dateForNextMonth = moment([year, currentMonth +1, nextMonthDay]);
+
+        let dayEl = createDayElement(dateForNextMonth);
+        dayEl.addClass('next-month');
+        monthEl.append(dayEl);
     }
 
     calendarEl.find('.months').append(monthEl);
+}
+
+function getLastMonth (year, month) {
+    let newMonth = month - 1
+
+    if (newMonth < 0) {
+        newMonth = 11
+        --year
+    }
+
+    return moment([year, newMonth])
 }
 
 function createDayElement (date) {
     let dayEl = $('<div/>').addClass('day');
     dayEl.data('date', date.format('YYYY-MM-DD'));
     dayEl.html(date.format('DD'));
+
+    dayEl.on('click', function (e) {
+        console.log($(this).data('date'));
+    });
 
     return dayEl;
 }
@@ -107,7 +139,6 @@ function findCalendarView(month, year) {
         }
     });
 
-    console.log(selectedMonth);
 
     return selectedMonth;
 }
